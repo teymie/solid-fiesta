ef0745ca1e701b64f26a2c30d63828a33f548ba9
teymie <65744646+teymie@users.noreply.github.com>
2022-06-20 17:50:07 -0700

    new change

--- I4G003194RDE/blog/models.py
+++ I4G003194RDE/blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     text = models.TextField()
-    author = models.ForeignKey(User , on_delete = models.CASCADE)
+    author = models.ForeignKey(get_user_model() , on_delete = models.CASCADE)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
         
